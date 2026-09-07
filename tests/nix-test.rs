6f38728f1d02ef29a77aa9cf099ee709ffa88a2f
yehpop <yusa@Yusas-MacBook-Pro.local>
2026-09-07 22:07:16 +0300

    Cant program rn

--- tests/nix-test.rs
+++ tests/nix-test.rs
@@ -3,7 +3,7 @@ use predicates::prelude::*;
 
 #[test]
 fn test_create_cellar() {
-    let cmd = Command::cargo_bin("cellars").unwrap();
+    let mut cmd = Command::cargo_bin("cellars").unwrap();
     let assert = cmd.arg("create").arg("test_cellar").assert();
-    assert.success().stdout(predicate::str::contains("Cellar 'test_cellar' created successfully")); 
+    assert.success().stdout(predicate::str::contains("Created environment cellar: test_cellar")); 
 }
